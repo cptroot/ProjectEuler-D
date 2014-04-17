b35c78ce1898616c81d1ddbd6a5d3752533f9c67
Evan Davis <cptroot@gmail.com>
2014-04-16 18:09:02 -0700

    Cleaned up Euler_10

--- Problem 10/Euler_10.d
+++ Problem 10/Euler_10.d
@@ -1,32 +1,10 @@
 import std.stdio;
 import std.math;
+import std.algorithm;
 
-//Sieve of Eratosthenes, includes limit
-bool[] Sieve(int limit) {
-  float sqrtLimit = sqrt(limit);
-  // true denotes composite number
-  bool[] sieve = new bool[limit - 1];
-  int[] edit = [];
-  for (auto index = 2; index < sqrtLimit; index++) {
-    if (!sieve[index - 2]) {
-      for (auto i = index; i <= limit / index; i++) {
-        if (!sieve[i - 2])
-          edit ~= [i * index - 2];
-      }
-      foreach (comp; edit)
-        sieve[comp] = true;
-    }
-  }
-  return sieve;
-}
+import sieve;
 
 void main() {
-  bool[] sieve = Sieve(2_000_000);
-  long sum = 0;
-  foreach (i, a; sieve) {
-    if (!a) {
-      sum += i + 2;
-    }
-  }
-  writeln(sum);
+  auto primes = ShortenSieve(Sieve(2_000_000));
+  writeln(reduce!((a, b) => a + b)(0L, primes));
 }
